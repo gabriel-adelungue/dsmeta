99e964a1edfee2ba30d9eeb194fc3482b8440c6e
Gabriel Adelungue <gabriel.adlungue@gmail.com>
2022-09-09 11:25:34 -0300

    other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import logo from '../../assets/img/logo.svg'
+
+import './styles.css'
+
+function Header() {
+    return(
+      <header>
+      <div className="dsmeta-logo-container">
+            <img src={logo} alt="Logo DSMeta" />
+            <h1>DSMeta</h1>
+            <p>Desenvolvido por <a href="https://www.instagram.com/biel_adelungue" target="_blank">@biel_adelungue</a></p>    
+        </div>
+      </header>
+    )
+  }
+  
+export default Header
+  
