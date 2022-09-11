b62cd2e1e8ca5f97cd45657bee927e256f5676ca
Gabriel Adelungue <gabriel.adlungue@gmail.com>
2022-09-11 19:24:23 -0300

    API test

---
+++ backend/src/main/java/com/gabrieladelungue/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.gabrieladelungue.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gabrieladelungue.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
---
+++ backend/src/main/java/com/gabrieladelungue/dsmeta/services/SaleService.java
@@ -0,0 +1,21 @@
+package com.gabrieladelungue.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabrieladelungue.dsmeta.entities.Sale;
+import com.gabrieladelungue.dsmeta.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	public List<Sale> findSales() {
+		return repository.findAll();
+	}
+
+}
